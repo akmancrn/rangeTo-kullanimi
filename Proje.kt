84311ab957c0d24e3377b850c8651923280d8e66
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 15:31:43 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+  var i: Int = 5
+
+   if(i in 1.rangeTo(20)){
+      print("i değeri bu aralıktadır")
+   }
+   else{
+      print("i değeri bu aralıkta değildir")
+   }
+
+}
